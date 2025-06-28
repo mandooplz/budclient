551460448bb859d47584b1216cd6eb0c3d1a6dcb
mandooplz <kmw4561@gmail.com>
2025-06-28 18:17:14 +0900

    [have to] add tests for EmailForm.signUpByCache when emailCredential is Nil

--- Tests/BudClientTests/AuthBoard/EmailFormTests.swift
+++ Tests/BudClientTests/AuthBoard/EmailFormTests.swift
@@ -69,8 +69,13 @@ struct EmailFormTests {
             await setEmailCredentialInBudCache(mode: .test)
         }
         
-        @Test func issueIsNotOccurred() async throws {
+        @Test func signInWithOutEmailAndPassword() async throws {
             // given
+            await MainActor.run {
+                emailFormRef.email = ""
+                emailFormRef.password = ""
+            }
+            
             try await #require(emailFormRef.isIssueOccurred == false)
             
             // when
@@ -78,6 +83,73 @@ struct EmailFormTests {
             
             // then
             await #expect(emailFormRef.issue == nil)
+        }
+        
+        @Test func setNilAuthBoardInBudCliet() async throws {
+            // when
+            await emailFormRef.signInByCache()
+            
+            // then
+            await #expect(emailFormRef.issue == nil)
+            try await #require(emailFormRef.isIssueOccurred == false)
+            await #expect(budClientRef.authBoard == nil)
+        }
+        
+        @Test func deleteEmailFormWhenSuccess() async throws {
+            // when
+            await emailFormRef.signInByCache()
+            
+            // then
+            try await #require(emailFormRef.isIssueOccurred == false)
+            
+            await #expect(EmailFormManager.get(emailFormRef.id) == nil)
+        }
+        @Test func deleteSignUpFormWhenSuccess() async throws {
+            // given
+            await emailFormRef.setUpSignUpForm()
+            let signUpForm = try #require(await emailFormRef.signUpForm)
+            try await #require(SignUpFormManager.get(signUpForm) != nil)
+
+            // when
+            await emailFormRef.signInByCache()
+            
+            // then
+            await #expect(SignUpFormManager.get(signUpForm) == nil)
+        }
+        @Test func deleteAuthBoardWhenSuccess() async throws {
+            // given
+            let authBoard = try #require(await budClientRef.authBoard)
+            
+            // when
+            await emailFormRef.signInByCache()
+            
+            // then
+            await #expect(AuthBoardManager.get(authBoard) == nil)
+        }
+        @Test func createProjectBoardWhenSuccess() async throws {
+            // when
+            await emailFormRef.signInByCache()
+            
+            // then
+            let projectBoard = try #require(await budClientRef.projectBoard)
+            await #expect(ProjectBoardManager.get(projectBoard) != nil)
+        }
+        @Test func createProfileBoardWhenSuccess() async throws {
+            // when
+            await emailFormRef.signInByCache()
+            
+            // then
+            let profileBoard = try #require(await budClientRef.profileBoard)
+            await #expect(ProfileBoardManager.get(profileBoard) != nil)
+        }
+        @Test func setIsUserSignedInWhenSuccess() async throws {
+            // when
+            await emailFormRef.signInByCache()
+            
+            // then
+            try await #require(emailFormRef.isIssueOccurred == false)
+            
+            await #expect(budClientRef.isUserSignedIn == true)
         }
     }
     
