2699d7087f544382a0e29c22a02366f279018444
kmw4561@gmail.com <kmw4561@gmail.com>
2025-07-05 00:21:58 +0900

    implement Flow, ObjectModel

--- Sources/BudClient/ProjectBoard/Project/Flow/Flow.swift
+++ Sources/BudClient/ProjectBoard/Project/Flow/Flow.swift
@@ -4,4 +4,53 @@
 //
 //  Created by 김민우 on 7/4/25.
 //
+import Foundation
+import Tools
 
+
+// MARK: Object
+@MainActor @Observable
+public final class Flow: Sendable {
+    // MARK: core
+    init() {
+        FlowManager.register(self)
+    }
+    func delete() {
+        FlowManager.unregister(self.id)
+    }
+    
+    // MARK: state
+    nonisolated let id = ID()
+    
+    
+    // MARK: action
+    
+    
+    // MARK: value
+    @MainActor
+    public struct ID: Sendable, Hashable {
+        let value: UUID
+        nonisolated init(value: UUID = UUID()) {
+            self.value = value
+        }
+        
+        var isExist: Bool {
+            FlowManager.container[self] != nil
+        }
+        public var ref: Flow? {
+            FlowManager.container[self]
+        }
+    }
+}
+
+// MARK: Flow Manager
+@MainActor @Observable
+fileprivate final class FlowManager: Sendable {
+    fileprivate static var container: [Flow.ID: Flow] = [:]
+    fileprivate static func register(_ object: Flow) {
+        container[object.id] = object
+    }
+    fileprivate static func unregister(_ id: Flow.ID) {
+        container[id] = nil
+    }
+}
--- Sources/BudClient/ProjectBoard/Project/ObjectModel/ObjectModel.swift
+++ Sources/BudClient/ProjectBoard/Project/ObjectModel/ObjectModel.swift
@@ -9,3 +9,53 @@ import Tools
 
 
 // MARK: Object
+@MainActor @Observable
+public final class ObjectModel: Sendable {
+    // MARK: core
+    init(object: ObjectID) {
+        self.object = object
+        
+        ObjectModelManager.register(self)
+    }
+    func delete() {
+        ObjectModelManager.unregister(self.id)
+    }
+    
+    
+    // MARK: state
+    nonisolated let id = ID()
+    nonisolated let object: ObjectID
+    
+    
+    // MARK: action
+    
+    
+    // MARK: value
+    @MainActor
+    public struct ID: Sendable, Hashable {
+        let value: UUID
+        nonisolated init(value: UUID = UUID()) {
+            self.value = value
+        }
+        
+        var isExist: Bool {
+            ObjectModelManager.container[self] != nil
+        }
+        var ref: ObjectModel? {
+            ObjectModelManager.container[self]
+        }
+    }
+}
+
+
+// MARK: Object Manager
+@MainActor @Observable
+fileprivate final class ObjectModelManager: Sendable {
+    fileprivate static var container: [ObjectModel.ID: ObjectModel] = [:]
+    fileprivate static func register(_ object: ObjectModel) {
+        container[object.id] = object
+    }
+    fileprivate static func unregister(_ id: ObjectModel.ID) {
+        container[id] = nil
+    }
+}
